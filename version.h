c44a8d957d2d9c647585342b514f70629f038eda
Filipe Martins <38554362+FilipeMartins2018@users.noreply.github.com>
2025-02-24 05:27:42 -0800

    Update version.h
    version indicates the fork

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 /* $OpenBSD: version.h,v 1.104 2025/02/15 01:52:07 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_9.9"
+#define SSH_VERSION	"OpenSSH_9.9-CybrAnalytiqa.com"
 
 #define SSH_PORTABLE	"p2"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
